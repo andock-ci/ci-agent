142c0f505c5fc7994bf1628b5994412122b99aef
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-02-13 23:45:35 +0100

    Use @ instead of $

--- 7.2/startup.sh
+++ 7.2/startup.sh
@@ -54,12 +54,10 @@ if [[ "$1" == "supervisord" ]]; then
 # Command mode (run as docker user)
 else
 	# This makes sure the environment is set up correctly for the docker user
-	# DOCKSALRC='source $HOME/.docksalrc >/dev/null 2>&1'
+	DOCKSALRC='source $HOME/.docksalrc >/dev/null 2>&1'
 	# Launch the passed command in an non-interactive bash session under docker user
 	# $@ does not work here. $* has to be used.
 
-	#exec gosu docker bash -c "$DOCKSALRC"
-	#source $HOME/.docksalrc;
+	exec gosu docker bash -c "$DOCKSALRC;$@"
 
-	gosu docker "$@"
 fi
